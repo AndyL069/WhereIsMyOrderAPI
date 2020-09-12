c9f3d828eb34e4af2baece52227f98bf70895fcb
Andreas Lichtsinn <alichtsinn@campana-schott.com>
2020-09-12 17:08:41 +0200

    Migrations

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using WhereIsMyOrderAPI.Models;
 
 namespace WhereIsMyOrderAPI
 {
     public class ApplicationDbContext : DbContext
     {
+        private IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public ApplicationDbContext()
+        {
+
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         { }
+
+        protected override void  OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(config.GetConnectionString("SqlConnectionString"));
 
         public DbSet<Order> Orders { get; set; }
     }
---
+++ Migrations/20200912150641_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhereIsMyOrderAPI.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Company = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Arrival = table.Column<DateTime>(nullable: false),
+                    Link = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<string>(nullable: true),
+                    TrackingNumber = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
